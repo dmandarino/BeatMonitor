35a3bf35b7364116ccb7fea1e33d10380d7cedd9
dmandarino <douglasmandarino@Douglass-MacBook-Pro.local>
2015-07-27 17:14:31 -0300

    continuando DAO

--- BeatMonitor/DAO.swift
+++ BeatMonitor/DAO.swift
@@ -10,19 +10,30 @@ import Foundation
 
 class DAO {
    
-    class func saveScheduleData(jsonString:User, key:String) {
+    class func saveUserData(jsonString:String) {
         
-        let path = getPath(key)
-        let dict: NSMutableDictionary = [key: jsonString]
+        let path = getPath("User")
+        let dict: NSMutableDictionary = ["User": jsonString]
         
         //saving values
-        dict.setObject(jsonString, forKey: key)
+        dict.setObject(jsonString, forKey: "User")
         dict.writeToFile(path, atomically: true)
     }
     
-    class func loadScheduleData(key:String) -> String{
+    class func saveResultsData(results:[Int]) {
+        
+        let path = getPath("Results")
+        let dict: NSMutableDictionary = ["Results": results]
+        
+        //saving values
+        dict.setObject(results, forKey: "Results")
+        dict.writeToFile(path, atomically: true)
+    }
+
+    
+    class func loadUserData() -> String{
    
-        let path = getPath(key)
+        let path = getPath("User")
         _ = NSFileManager.defaultManager()
         //check if file exists
 //        if(!fileManager.fileExistsAtPath(path)) {
@@ -41,7 +52,7 @@ class DAO {
         
         let myDict = NSDictionary(contentsOfFile: path)
         if let dict = myDict {
-            let schedules: AnyObject? = dict[key]
+            let schedules: AnyObject? = dict["User"]
             if schedules?.description != nil{
                 return schedules!.description
             }
@@ -50,6 +61,25 @@ class DAO {
         }
         return ""
     }
+    
+    
+//    class func loadUserData() -> [Int]{
+//        
+//        let path = getPath("Results")
+//        _ = NSFileManager.defaultManager()
+//        
+//        _ = NSMutableDictionary(contentsOfFile: path)
+//        let myDict = NSDictionary(contentsOfFile: path)
+//        if let dict = myDict {
+//            let results: AnyObject? = dict["Results"]
+//            if results?.description != nil {
+//                return results!.description
+//            }
+//        } else {
+//            print("WARNING: Couldn't create dictionary from plist! Default values will be used!")
+//        }
+//        return [0,0,0,0,0,0,0,0,0,0]
+//    }
     
     private class func getPath(key:String) -> String{
         let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as NSArray
